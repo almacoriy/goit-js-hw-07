7d1ebfc621c8384bde536d2b8fa1099728b4e8eb
almacoryi <almacoriy@gmail.com>
2021-12-06 22:37:20 +0200

    add gallery cards

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+								<a class="gallery__link" href="${preview}">
+									<img
+										class="gallery__image"
+										src="${preview}"
+										data-source="large-image.jpg"
+										alt="${description}"
+									/>
+								</a>
+							</div>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
