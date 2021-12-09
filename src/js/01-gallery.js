d30c881134809bc32be97fc897848b9012ed454f
almacoryi <almacoriy@gmail.com>
2021-12-09 23:23:35 +0200

    add task -01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 import { galleryItems } from "./gallery-items.js";
 
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="" width="400" height="200">
+    </div>
+`);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+  galleryItem: document.querySelector(".gallery__item"),
+  linkImage: document.querySelector(".gallery__link"),
+  image: document.querySelector(".gallery__image"),
+  originalImage: document.querySelector("img[data-source]"),
+  modalImage: instance.element().querySelector("img"),
+};
+
+//===== Создаем галерею из массива =====
 const galleryContainer = document.querySelector(".gallery");
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 
@@ -9,11 +25,11 @@ function createGalleryCardsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-								<a class="gallery__link" href="${preview}">
+								<a class="gallery__link" href="${original}">
 									<img
 										class="gallery__image"
 										src="${preview}"
-										data-source="large-image.jpg"
+										data-source="${original}"
 										alt="${description}"
 									/>
 								</a>
@@ -22,4 +38,22 @@ function createGalleryCardsMarkup(items) {
     .join("");
 }
 
-console.log(galleryItems);
+//=====  =====
+
+refs.gallery.addEventListener("click", onModalOpen);
+refs.modalImage.addEventListener("click", onModalClose);
+
+function onModalOpen(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") return;
+  refs.modalImage.src = event.target.dataset.source;
+
+  instance.show();
+}
+
+function onModalClose() {
+  instance.close();
+}
+
+// console.log(galleryI?tems);
